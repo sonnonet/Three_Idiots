35ff1722aaf223f4dbcf1d64e7df67855feb3875
vividswan <vividswan@naver.com>
2020-08-26 03:10:09 +0900

    Create ISQLite.cs

---
+++ tizen-project/Three_Idiots_Project/Three_Idiots_Project/Three_Idiots_Project/ISQLite.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Three_Idiots_Project
+{
+    public interface ISQLite
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
