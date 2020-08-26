6df09d8df3cc0b106293acfd9e7058ac04933f69
vividswan <vividswan@naver.com>
2020-08-27 02:53:46 +0900

    Create Three_Idiots.cs

---
+++ tizen-project/Three_Idiots_Project/Three_Idiots_Project/Three_Idiots_Project/Models/Three_Idiots.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Three_Idiots.Models
+{
+    public class Three_Idiots
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
