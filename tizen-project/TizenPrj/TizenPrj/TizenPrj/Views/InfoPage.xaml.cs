0d67accaa78a8ea3d9327f5906954ef129cb6b4d
vividswan <vividswan@naver.com>
2020-09-05 13:40:09 +0900

    Update InfoPage.xaml.cs

--- tizen-project/TizenPrj/TizenPrj/TizenPrj/Views/InfoPage.xaml.cs
+++ tizen-project/TizenPrj/TizenPrj/TizenPrj/Views/InfoPage.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace TizenPrj.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class InfoPage : ContentView
+    public partial class InfoPage : ContentPage
     {
         public InfoPage()
         {
