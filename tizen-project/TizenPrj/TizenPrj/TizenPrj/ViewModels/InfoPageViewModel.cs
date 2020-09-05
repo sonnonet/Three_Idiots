c680b8e71c1cef4b457bdb08db19a9ae6007fbf8
vividswan <vividswan@naver.com>
2020-09-05 13:40:17 +0900

    Update InfoPageViewModel.cs

--- tizen-project/TizenPrj/TizenPrj/TizenPrj/ViewModels/InfoPageViewModel.cs
+++ tizen-project/TizenPrj/TizenPrj/TizenPrj/ViewModels/InfoPageViewModel.cs
@@ -1,22 +1,46 @@
 ﻿using System;
+using TizenPrj.models;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
 using System.Text;
-
-using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace TizenPrj.ViewModels
 {
-    public class InfoPageViewModel : ContentView
+    class InfoPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private List<InfoModel> _dataList;
+        public List<InfoModel> DataList
+        {
+            get { return _dataList; }
+            set
+            {
+                _dataList = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DataList"));
+            }
+        }
+
         public InfoPageViewModel()
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
-            };
+            _ = GetInfoData();
         }
+
+        private async Task GetInfoData()
+        {
+            var json = await new HttpClient().GetStringAsync("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=ng%2FLijSMgnWyRTRqAAQMqwcyEEt5yqOqnkeEGn1oMGfqIEaP492zeRvX4%2BBnK9Zn4woeMLrSj21o6WToZ%2BOpxQ%3D%3D&numOfRows=10&pageNo=1&sidoName=%EC%9D%B8%EC%B2%9C&ver=1.3&_returnType=json");
+            var data = InfoModel.FromJson(json);
+            //var jdata = (JArray)data["list"];
+            DataList = new List<InfoModel>(data);
+        }
+
     }
+
+
+
 }
